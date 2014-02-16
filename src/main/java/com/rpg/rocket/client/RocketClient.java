624bacc0943f5f1317d4e64608df32dfb6ec5a7c
sunnykaka <sunnykaka@vip.qq.com>
2014-02-16 19:25:16 +0800

    init

--- src/main/java/com/rpg/rocket/client/RocketClient.java
+++ src/main/java/com/rpg/rocket/client/RocketClient.java
@@ -1,6 +1,8 @@
 package com.rpg.rocket.client;
 
 import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelOption;
@@ -8,6 +10,8 @@ import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
+
+import java.io.UnsupportedEncodingException;
 
 public class RocketClient {
     public static void main(String[] args) throws Exception {
@@ -32,7 +36,13 @@ public class RocketClient {
             // Start the client.
             ChannelFuture f = b.connect(host, port).sync();
 
-            f.channel().writeAndFlush("hello world");
+            ByteBuf firstMessage = Unpooled.buffer(64);
+            try {
+                firstMessage.writeBytes("你好啊".getBytes("UTF-8"));
+                f.channel().writeAndFlush(firstMessage);
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
             // Wait until the connection is closed.
             f.channel().closeFuture().sync();
         } finally {
--- src/main/java/com/rpg/rocket/client/RocketClientHandler.java
+++ src/main/java/com/rpg/rocket/client/RocketClientHandler.java
@@ -1,9 +1,12 @@
 package com.rpg.rocket.client;
 
 import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.util.CharsetUtil;
+
+import java.io.UnsupportedEncodingException;
 
 public class RocketClientHandler extends ChannelInboundHandlerAdapter {
 
@@ -17,6 +20,23 @@ public class RocketClientHandler extends ChannelInboundHandlerAdapter {
             m.release();
         }
     }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) {
+        System.out.println("actived");
+//        ByteBuf firstMessage = Unpooled.buffer(64);
+//        try {
+//            firstMessage.writeBytes("你好啊".getBytes("UTF-8"));
+//            ctx.writeAndFlush(firstMessage);
+//        } catch (UnsupportedEncodingException e) {
+//            e.printStackTrace();
+//        }
+    }
+
+//    @Override
+//    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+//        ctx.flush();
+//    }
 
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
